4d89e77aacf799ffb61e3657c5d275465892db62
Alexander O. Anisimov <anisimov2004@gmail.com>
2010-03-02 00:33:17 +0500

    Add requestType MegaProtocol::GET_RECORDS_LIST

--- src/megasocket.cpp
+++ src/megasocket.cpp
@@ -3,7 +3,8 @@
 #include "table.h"
 #include "megaprotocol.h"
 
-MegaSocket::MegaSocket(QObject *parent) : QTcpSocket(parent)
+MegaSocket::MegaSocket(QObject *parent) :
+    QTcpSocket(parent)
 {
     nextBlockSize = 0;
     connect(this, SIGNAL(readyRead()), this, SLOT(readClient()));
@@ -26,21 +27,41 @@ void MegaSocket::readClient()
     if (requestType == MegaProtocol::GET_TABLES_LIST)
     {
         QList<Table> tables = data->getTables();
-        QByteArray block;
-        QDataStream out(&block, QIODevice::WriteOnly);
-        out.setVersion(QDataStream::Qt_4_5);
         for (int i = 0; i < tables.length(); ++i)
         {
-            out << quint16(0);
-            out << tables[i].getName() << tables[i].getComment();
+            QByteArray block;
+            QDataStream out(&block, QIODevice::WriteOnly);
+            out.setVersion(QDataStream::Qt_4_5);
+            out << quint16(0) << tables[i].getName() << tables[i].getComment();
             out.device()->seek(0);
             out << quint16(block.size() - sizeof(quint16));
-            out.device()->seek(0);
             write(block);
         }
-        out << quint16(0xFFFF);
-        write(block);
     }
+    if (requestType == MegaProtocol::GET_RECORDS_LIST)
+    {
+        QString tableName;
+        in >> tableName;
+        QList<Record> records = data->getTable(tableName)->getRecords();
+        for (int i = 0; i < records.length(); ++i)
+        {
+            QString readOnly = records[i].isReadOnly() ? "true" : "false";
+            QString type = records[i].getType() == QVariant::Double ? "double"
+                           : records[i].getType() == QVariant::Int ? "int"
+                           : "string";
+            QByteArray block;
+            QDataStream out(&block, QIODevice::WriteOnly);
+            out.setVersion(QDataStream::Qt_4_5);
+            out << quint16(0) << records[i].getTitle()
+                    << records[i].getComment() << readOnly << type
+                    << records[i].getValue().toString();
+            out.device()->seek(0);
+            out << quint16(block.size() - sizeof(quint16));
+            write(block);
+        }
+    }
+    QDataStream out(this);
+    out << quint16(0xFFFF);
     close();
     syslog(LOG_DEBUG, "Leave: MegaSocket::readClient");
 }
