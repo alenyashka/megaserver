9d2686d12e5bae96ec4092c59a6035e51f62a72c
Alexander O. Anisimov <anisimov2004@gmail.com>
2010-03-05 00:28:10 +0500

    Add request type MegaProtocol::ADD_TABLE

--- src/megaprotocol.h
+++ src/megaprotocol.h
@@ -5,7 +5,8 @@ class MegaProtocol {
     public:
     enum Commands {
         GET_TABLES_LIST,
-        GET_RECORDS_LIST
+        GET_RECORDS_LIST,
+        ADD_TABLE
     };
 };
 #endif // MEGAPROTOCOL_H
