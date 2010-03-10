32f2931febae671be2c5dd5052808d4793db62f8
Alexander O. Anisimov <anisimov2004@gmail.com>
2010-03-11 01:21:04 +0500

    Update megaprotocol.h

--- src/megaprotocol.h
+++ src/megaprotocol.h
@@ -8,7 +8,15 @@ class MegaProtocol {
         GET_RECORDS_LIST,
         ADD_TABLE,
         EDIT_TABLE,
-        DEL_TABLE
+        DEL_TABLE,
+        ADD_RECORD,
+        EDIT_RECORD,
+        DEL_RECORD
+    };
+    enum Errors {
+        TABLE_EXIST,
+        RECORD_EXIST,
+        RECORD_IS_READ_ONLY
     };
 };
 #endif // MEGAPROTOCOL_H
